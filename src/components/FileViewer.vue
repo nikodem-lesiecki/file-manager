913885cabae27f5ae44f5895d90b339d7b32aa0b
nikodem-lesiecki <nlesiecki@icloud.com>
2024-07-27 15:52:10 +0200

    updated fileviewer and sidebar

--- src/components/FileViewer.vue
+++ src/components/FileViewer.vue
@@ -2,18 +2,25 @@
     <div
         class="file-viewer bg-black/20 rounded-3xl backdrop-blur-md shadow-lg ring-1 ring-black/5 p-4 flex justify-center items-center"
     >
-        <div class="text-white">No file selected</div>
+        <div class="text-white"></div>
     </div>
 </template>
 
 <script>
 export default {
     name: "FileViewer",
+    props: {
+        selectedFile: {
+            type: Object,
+        },
+    },
 };
 </script>
 
 <style>
 .file-viewer {
+    flex: 1;
+    min-height: 100vh;
     margin-top: 15px;
 }
 </style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,33 +1,28 @@
+<!-- Sidebar.vue -->
 <template>
     <aside
         class="sidebar bg-black/20 rounded-3xl backdrop-blur-md shadow-lg ring-1 ring-black/5 p-4"
     >
         <ul class="space-y-4">
-            <li>
-                <a href="#" class="block text-white hover:text-gray-400">
-                    Home
-                </a>
-            </li>
-            <li>
-                <a href="#" class="block text-white hover:text-gray-400">
-                    My Files
-                </a>
-            </li>
-            <li>
-                <a href="#" class="block text-white hover:text-gray-400">
-                    Shared
-                </a>
-            </li>
-            <li>
-                <a href="#" class="block text-white hover:text-gray-400">
-                    Trash
-                </a>
-            </li>
-            <li>
-                <a href="#" class="block text-white hover:text-gray-400">
-                    Settings
-                </a>
-            </li>
+            <template v-for="file in fs" :key="file.name">
+                <li @click="selectFile(file)" class="cursor-pointer">
+                    <a class="block text-white hover:text-gray-400">{{
+                        file.name
+                    }}</a>
+                </li>
+                <ul v-if="file.directory" class="ml-4 space-y-2">
+                    <li
+                        v-for="subFile in file.directory"
+                        :key="subFile.name"
+                        @click.stop="selectFile(subFile)"
+                        class="cursor-pointer"
+                    >
+                        <a class="block text-white hover:text-gray-400">{{
+                            subFile.name
+                        }}</a>
+                    </li>
+                </ul>
+            </template>
         </ul>
     </aside>
 </template>
@@ -35,6 +30,48 @@
 <script>
 export default {
     name: "Sidebar",
+    data() {
+        return {
+            fs: [
+                {
+                    name: "main.js",
+                    content: "console.log('hello')",
+                },
+                {
+                    name: "index.html",
+                    content: "<html></html>",
+                },
+                {
+                    name: "components",
+                    directory: [
+                        {
+                            name: "button.js",
+                            content:
+                                "export function Button() { return <button>test</button> }",
+                        },
+                        {
+                            name: "table",
+                            directory: [{ name: "table.js", content: "" }],
+                        },
+                        {
+                            name: "subdir",
+                            directory: [
+                                {
+                                    name: "haha.txt",
+                                    content: "haha lol",
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        };
+    },
+    methods: {
+        selectFile(file) {
+            this.$emit("file-selected", file);
+        },
+    },
 };
 </script>
 
